ae95eb589a5971c9a945c4c2648018eceb682cd0
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-01-27 17:38:48 +0100

    All About React Router

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import Router from "./components/Router";
+import "./css/style.css";
 
-import './css/style.css';
-import App from './components/App';
-import StorePicker from './components/StorePicker';
-import NotFound from './components/NotFound';
-
-const Root = () => {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact="true" path="/" component={StorePicker} />
-        <Route path="/store/:storeId" component={App} />
-        <Route component={NotFound} /> 
-      </Switch>
-    </BrowserRouter>
-  )
-}
-
-render(<Root />, document.getElementById('main'));
+render(<Router />, document.querySelector("#main"));
