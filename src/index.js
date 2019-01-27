816f8358eb1ccbf294b4024f2021729a1f24d507
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2019-01-27 13:29:08 +0100

    Routing with React Router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './css/style.css';
 import App from './components/App';
+import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-// import StorePicker from './components/StorePicker';
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact="true" path="/" component={StorePicker} />
+        <Route path="/store/:storeId" component={App} />
+        <Route component={NotFound} /> 
+      </Switch>
+    </BrowserRouter>
+  )
+}
 
-render(<App />, document.getElementById('main'));
+render(<Root />, document.getElementById('main'));
