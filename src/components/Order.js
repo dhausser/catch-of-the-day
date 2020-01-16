cee06f4cd35ceb4ccdc097918366948373924bd2
Davy Hausser <24432220+dhausser@users.noreply.github.com>
2020-01-16 22:19:29 +0100

    order page fixed

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,37 +1,36 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { formatPrice } from "../helpers";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import { formatPrice } from '../helpers';
 
-class Order extends React.Component {
-  // static propTypes = {
-  //   fishes: PropTypes.object,
-  //   order: PropTypes.object,
-  //   removeFromOrder: PropTypes.func
-  // };
-  renderOrder = key => {
-    const fish = this.props.fishes[key];
-    const count = this.props.order[key];
-    const isAvailable = fish && fish.status === "available";
-    const transitionOptions = {
-      classNames: "order",
-      key,
-      timeout: { enter: 500, exit: 500 }
-    };
+function Order({ fishes, order, removeFromOrder }) {
+  const renderOrder = (key) => {
+    const fish = fishes[key];
+    const count = order[key];
+    const isAvailable = fish && fish.status === 'available';
+
     // Make sure the fish is loaded before we continue!
     if (!fish) return null;
 
     if (!isAvailable) {
       return (
-        <CSSTransition {...transitionOptions}>
-          <li key={key}>
-            Sorry {fish ? fish.name : "fish"} is no longer available
+        <CSSTransition
+          classNames="order"
+          key={key}
+          timeout={{ enter: 500, exit: 500 }}
+        >
+          <li keyy={key}>
+            {`Sorry ${fish ? fish.name : 'fish'} is no longer available`}
           </li>
         </CSSTransition>
       );
     }
     return (
-      <CSSTransition {...transitionOptions}>
+      <CSSTransition
+        classNames="order"
+        key={key}
+        timeout={{ enter: 500, exit: 500 }}
+      >
         <li key={key}>
           <span>
             <TransitionGroup component="span" className="count">
@@ -40,43 +39,51 @@ class Order extends React.Component {
                 key={count}
                 timeout={{ enter: 500, exit: 500 }}
               >
-                <span>{count}</span>
+                <span key={count}>{count}</span>
               </CSSTransition>
             </TransitionGroup>
-            lbs {fish.name}
+            lbs
+            {' '}
+            {fish.name}
+          </span>
+          <span className="price">
             {formatPrice(count * fish.price)}
-            <button onClick={() => this.props.removeFromOrder(key)}>
-              &times;
-            </button>
+            <button type="button" onClick={() => removeFromOrder(key)}>&times;</button>
           </span>
         </li>
       </CSSTransition>
     );
   };
-  render() {
-    const orderIds = Object.keys(this.props.order);
-    const total = orderIds.reduce((prevTotal, key) => {
-      const fish = this.props.fishes[key];
-      const count = this.props.order[key];
-      const isAvailable = fish && fish.status === "available";
-      if (isAvailable) {
-        return prevTotal + count * fish.price;
-      }
-      return prevTotal;
-    }, 0);
-    return (
-      <div className="order-wrap">
-        <h2>Order</h2>
-        <TransitionGroup component="ul" className="order">
-          {orderIds.map(this.renderOrder)}
-        </TransitionGroup>
+
+  const orderIds = Object.keys(order);
+  const total = orderIds.reduce((prevTotal, key) => {
+    const fish = fishes[key];
+    const count = order[key];
+    const isAvailable = fish && fish.status === 'available';
+    if (isAvailable) {
+      return prevTotal + count * fish.price;
+    }
+    return prevTotal;
+  }, 0);
+
+  return (
+    <div className="order-wrap">
+      <h2>Order</h2>
+      <TransitionGroup component="ul" className="order">
+        {orderIds.map(renderOrder)}
         <div className="total">
           Total:
           <strong>{formatPrice(total)}</strong>
         </div>
-      </div>
-    );
-  }
+      </TransitionGroup>
+    </div>
+  );
 }
+
+Order.propTypes = {
+  fishes: PropTypes.objectOf.isRequired,
+  order: PropTypes.objectOf.isRequired,
+  removeFromOrder: PropTypes.func.isRequired,
+};
 
 export default Order;
